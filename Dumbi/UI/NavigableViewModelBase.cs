2fc0e775fb1708ab9dbab50222a72204a4e977d2
dsiddananja <deepak.siddananja@gmail.com>
2015-11-06 21:37:49 +0000

    Trivial - Added documentation

--- Dumbi/UI/NavigableViewModelBase.cs
+++ Dumbi/UI/NavigableViewModelBase.cs
@@ -3,20 +3,39 @@
     using Prism.Regions;
     using System.ComponentModel.Composition;
 
+    /// <summary>
+    /// View Model base that has Prism navigation enabled
+    /// </summary>
     public class NavigableViewModelBase : DisposableBindableBase, INavigationAware
     {
+        /// <summary>
+        /// The region manager
+        /// </summary>
         [Import]
         protected IRegionManager regionManager;
 
+        /// <summary>
+        /// Check interface documentation
+        /// </summary>
+        /// <param name="navigationContext"></param>
         public virtual void OnNavigatedTo(NavigationContext navigationContext)
         {
         }
 
+        /// <summary>
+        /// Check interface documentation
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        /// <returns></returns>
         public virtual bool IsNavigationTarget(NavigationContext navigationContext)
         {
             return true;
         }
 
+        /// <summary>
+        /// Check interface documentation
+        /// </summary>
+        /// <param name="navigationContext"></param>
         public virtual void OnNavigatedFrom(NavigationContext navigationContext)
         {
         }
--- Dumbi/UI/ViewModelBase.cs
+++ Dumbi/UI/ViewModelBase.cs
@@ -1,7 +1,13 @@
 ﻿namespace Dumbi.UI
 {
+    /// <summary>
+    /// Standard base for all view models
+    /// </summary>
     public class ViewModelBase : NavigableViewModelBase
     {
+        /// <summary>
+        /// Gets a value indicating whether this instance has any pending changes
+        /// </summary>
         public bool HasChanges { get; protected set; }
     }
 }
